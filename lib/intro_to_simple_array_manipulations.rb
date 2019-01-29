1ef553be52affce86b98cd55cf1e5cce69a247d1
Luiz Pereira <luizfper@gmail.com>
2019-01-29 20:26:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,17 @@
 def using_push(array, element)
-  updated_array=array.push(element  )
+  updated_array=array.push(element)
 end
   
+def using_unshift(array, element)
+  updated_array=array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array,tmes)
+  tmes.times do
+    array.pop
+  end
+end
